43dd0a7e15f0aa42e2f0b4d319f847213714e356
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-03-06 22:38:03 -0300

    atvrepetiçãowhile

---
+++ atvrepetiçãowhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n, i = 0;
+    printf("Digite quantas vezes você quer que repita");
+    scanf("%i", &n);
+    while (i < n){
+        printf("Eu adoro programar.\n");
+        printf("Eu adoro programar.\n");
+        i++;
+    }
+return 0;
+}
